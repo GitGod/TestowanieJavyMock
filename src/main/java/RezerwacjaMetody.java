a853ff9d0b3ce5907b9019277a7df8cd711da20e
GitGod <barmazu23@gmail.com>
2019-05-05 08:56:41 +0200

    Update RezerwacjaMetody.java

--- src/main/java/RezerwacjaMetody.java
+++ src/main/java/RezerwacjaMetody.java
@@ -63,7 +63,6 @@ public class RezerwacjaMetody {
                         }
                     }
                 }
-                generujPotwierdzenie(osoba, rezerwacja);
 
                 return rezerwacjeRepo.dodajRezerwacje(osoba,rezerwacja,restauracja);
             } else {
@@ -81,7 +80,6 @@ public class RezerwacjaMetody {
                         }
                     }
                 }
-                generujPotwierdzenie(osoba, rezerwacja);
                 return rezerwacjeRepo.dodajRezerwacje(osoba,rezerwacja,restauracja);
             }
         }
@@ -101,72 +99,7 @@ public class RezerwacjaMetody {
     }
 
 
-    public void generujDane(String filePath) throws IOException {
-        FileReader fileReader = new FileReader(filePath);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        HashMap<String, Integer> emaile = new HashMap();
-        HashMap<String, Integer> dni = new HashMap();
-        HashMap<Integer, Integer> godziny = new HashMap();
-        HashMap<Integer, Integer> stoliki = new HashMap();
 
-        String line = bufferedReader.readLine();
-        Restauracja res = new Restauracja();
-        // res.readFile("Restauracja.txt");
-        plik = false;
-        LocalDate data;
-        while (line != null) {
-            String[] podzielone = line.split(" ");
-            String[] podzielone2 = podzielone[8].split("-");
-            String[] podzielone3 = podzielone[6].split("-");
-            if (emaile.containsKey(podzielone[0])) {
-                emaile.put(podzielone[0], emaile.get(podzielone[0]) + 1);
-            } else {
-                emaile.put(podzielone[0], 1);
-            }
-            if (godziny.containsKey(Integer.parseInt(podzielone2[0]))) {
-                godziny.put(Integer.parseInt(podzielone2[0]), godziny.get(Integer.parseInt(podzielone2[0])) + 1);
-            } else {
-                godziny.put(Integer.parseInt(podzielone2[0]), 1);
-
-            }
-            if (stoliki.containsKey(Integer.parseInt(podzielone[3]))) {
-                stoliki.put(Integer.parseInt(podzielone[3]), stoliki.get(Integer.parseInt(podzielone[3])) + 1);
-            } else {
-                stoliki.put(Integer.parseInt(podzielone[3]), 1);
-
-            }
-            data = LocalDate.of(Integer.parseInt(podzielone3[0])
-                    , Integer.parseInt(podzielone3[1]),
-                    Integer.parseInt(podzielone3[2]));
-            if (dni.containsKey(data.getDayOfWeek().toString())) {
-                dni.put(data.getDayOfWeek().toString(), dni.get(data.getDayOfWeek().toString()) + 1);
-            } else {
-                dni.put(data.getDayOfWeek().toString(), 1);
-
-            }
-            line = bufferedReader.readLine();
-        }
-        PrintWriter plik2 = null;
-        String najczestszyEmail = Collections.max(emaile.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-        String najczestszyDzien = Collections.max(dni.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-        Integer najczestszaGodzina = Collections.max(godziny.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-        Integer najczestszyStolik = Collections.max(stoliki.entrySet(), (entry1, entry2) -> entry1.getValue() - entry2.getValue()).getKey();
-        try {
-            plik2 = new PrintWriter(new FileWriter("plik2.txt"));
-            plik2.print("Najczesciej rezerwujacy ");
-            plik2.println(najczestszyEmail + " " + emaile.get(najczestszyEmail));
-            plik2.print("Najwiecej rezerwacji w dniu ");
-            plik2.println(najczestszyDzien + " " + dni.get(najczestszyDzien));
-            plik2.print("Najczesciej rezerwowana godzina ");
-            plik2.println(najczestszaGodzina + " " + godziny.get(najczestszaGodzina));
-            plik2.print("Najczesciej rezerwowany stolik ");
-            plik2.println(najczestszyStolik + " " + stoliki.get(najczestszyStolik));
-        } finally {
-            if (plik2 != null) {
-                plik2.close();
-            }
-        }
-    }
 
 
     public boolean compare(Object o1, Object o2) {
@@ -190,34 +123,11 @@ public class RezerwacjaMetody {
 
     }
 
-    public void generujPotwierdzenie(Osoba osoba, Rezerwacja rezerwacja) throws IOException {
-        PrintWriter potwierdzenie = null;
 
-        try {
-            potwierdzenie = new PrintWriter(new FileWriter("potwierdzenie.txt"));
-            potwierdzenie.println("Brawo udalo ci sie zarezerwowac stolik w naszej restauracji! ");
-            potwierdzenie.println("Rezerwacja na :\nimie: " + osoba.getImie() + " email: " + osoba.getEmail());
-            potwierdzenie.print("Informacje o rezerwacji: ");
-            potwierdzenie.println(rezerwacja.data + " " + rezerwacja.godzina + "-" + rezerwacja.godzinaDo + " Stolik numer: " + rezerwacja.stolik.numer + " Liczba miejsc: " + rezerwacja.stolik.liczbaMiejsc);
-        } finally {
-            if (potwierdzenie != null) {
-                potwierdzenie.close();
-            }
-        }
-    }
-
-    public String generujPotwierdzenie2(Osoba osoba, Rezerwacja rezerwacja) throws IOException {
-        String potwierdzenie = "";
-        potwierdzenie = potwierdzenie.concat("Brawo udalo ci sie zarezerwowac stolik w naszej restauracji!\n ");
-        potwierdzenie = potwierdzenie.concat("Rezerwacja na :\nimie: " + osoba.getImie() + " email: " + osoba.getEmail() + "\n");
-        potwierdzenie = potwierdzenie.concat("Informacje o rezerwacji: ");
-        potwierdzenie = potwierdzenie.concat(rezerwacja.data + " " + rezerwacja.godzina + "-" + rezerwacja.godzinaDo + " Stolik numer: " + rezerwacja.stolik.numer + " Liczba miejsc: " + rezerwacja.stolik.liczbaMiejsc);
-        return potwierdzenie;
-    }
 
     public boolean sprawdzCzyotwarte(Rezerwacja rezerwacja, Restauracja restauracja) {
         Calendar c = Calendar.getInstance();
-        //  c.setTime(rezerwacja.data);
+
         int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
         String godzinki = restauracja.godziny_otwarcia.get(rezerwacja.data.getDayOfWeek().toString());
         String podzielone[] = godzinki.split("-");
